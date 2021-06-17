23648af3d4a9789194ac228f4cc9594f5d014917
Lizandra Lígia <lizandraligia@hotmail.com>
2021-06-16 21:22:57 -0300

    Fixes in show details screen

--- webapp/src/pages/showDetails/showDetails.js
+++ webapp/src/pages/showDetails/showDetails.js
@@ -1,11 +1,13 @@
 import Button from '../../components/Button';
 import CategoryLabel from '../../components/CategoryLabel';
+import RatingCard from '../../components/RatingCard';
 
 export default {
   name: 'showDetails',
   components: {
     'Button': Button,
-    'CategoryLabel': CategoryLabel
+    'CategoryLabel': CategoryLabel,
+    'RatingCard': RatingCard
   },
   props: [],
   data () {
@@ -16,10 +18,17 @@ export default {
 
   },
   mounted () {
-
   },
   methods: {
+    showEvaluations() {
+      document.getElementById("right").style.display = "none";
+      document.getElementById("evaluations").style.display = "block";
+    },
 
+    hideEvaluations() {
+      document.getElementById("right").style.display = "block";
+      document.getElementById("evaluations").style.display = "none";
+    }
   }
 }
 
