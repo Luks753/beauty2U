65ee9602a7f9f036f8df9bc345572f4f209c87f1
Matheus Cavalcanti <mthcavalcanti@gmail.com>
2021-07-03 00:28:27 -0300

    [BACKEND] Create migration for cpf, cnpj and overall average/[FRONTEND] Split cpf and cnpj field in two different ones

---
+++ api/src/database/migrations/20210703012221_alter-professional-add-cpfcnpj.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.table('professionals', function(table){
+        table.string('cpf').unique();
+        table.string('cnpj').unique();
+        table.decimal('average',7,2);
+      })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropColumns(['cpf', 'cnpj', 'average']);
+};
